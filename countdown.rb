46a7cceb77b36f3cff871077d9b09c025beae3bb
Rachel Katz <rachel.katz@flatironschool.com>
2022-05-12 16:03:43 -0400

    commit tree e47a979d6a83c387f2b84f8e7065482dbcab24b9

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
 
-def countdown
+def countdown(x)
+
+    while x >= 1
+        puts "#{x} SECOND(S)!"
+        x -= 1
+    end 
+    return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(x)
+    while x >= 1
+        sleep(1)
+        puts "#{x} SECOND(S)!"
+        x -= 1
+    end 
+    return "HAPPY NEW YEAR!"
+end 
