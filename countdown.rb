709ceed67f549ab97b3e516034b7f932034e63f6
Isaiah <ijandicos90@gmail.com>
2022-07-13 21:46:50 -0400

    Completed Assignment

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
 #write your code here
 
-def countdown
+def countdown(num)
+    num = 10
+    while num > 0
+      puts "#{num} SECOND(S)!"
+      num -= 1
+    end
+    puts "HAPPY NEW YEAR!"
 end
+
+
+countdown(10)
